81c882c9034d7025356dcc0be45244346def010b
Tristan <91071526+TristanKM@users.noreply.github.com>
2023-10-15 16:57:37 +0200

    Update script.js

--- Tueur/script.js
+++ Tueur/script.js
@@ -1,6 +1,7 @@
 //Exo2
 //Creation du tueur
-let jason = {
+let jason = 
+{
     nom: 'Jason',
     sante: 100,
     probMourir: 0.2,
@@ -17,38 +18,39 @@ const caracteristiques = ['Nerd', 'Sportif', 'Blonde', 'Intelligent', 'Courageux
 const nomsSurvivants = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Helen', 'Ivy', 'Jack'];
 
 //Fonction nom aleatoire
-function getRandomName() {
+function getRandomName() 
+{
     return nomsSurvivants[Math.floor(Math.random() * nomsSurvivants.length)];
 }
 
 //Fonction attribuation des caracteristiques au survivants choisi aleatoirement
-function getRandomCharacteristic() {
+function getRandomCharacteristic() 
+{
     return caracteristiques[Math.floor(Math.random() * caracteristiques.length)];
 }
 
 //Fonction pour ajouter un message au contenu HTML
-function logMessage(message) {
+function logMessage(message) 
+{
     const output = document.getElementById('output');
     output.innerHTML += message + '<br>';
 }
 
-//Fonction qui permet d'ajouter des icons des resultats
-function logResult(result) {
-    const resultIcons = document.getElementById('result-icons');
-    resultIcons.innerHTML += result;
-}
-
 //Fonction permettant de simuler l'attaque d'un survivants 
-function attackSurvivant(survivant) {
+function attackSurvivant(survivant) 
+{
     const attaqueResultat = Math.random();
     
-    if (attaqueResultat <= jason.probMourir) {
+    if (attaqueResultat <= jason.probMourir) 
+    {
         logMessage(`Jason a tué ${survivant.nom}`);
         return 'mort';
-    } else if (attaqueResultat <= jason.probMourir + jason.probInfligerDegats) {
+    } else if (attaqueResultat <= jason.probMourir + jason.probInfligerDegats) 
+    {
         logMessage(`${survivant.nom} a esquivé et a infligé 10 points de dégâts à Jason`);
         jason.sante -= 10;
-    } else {
+    } else 
+    {
         logMessage(`${survivant.nom} a infligé 15 points de dégâts à Jason mais est mort en le faisant`);
         jason.sante -= 15;
         return 'mort';
@@ -56,36 +58,41 @@ function attackSurvivant(survivant) {
 }
 
 //Fonction permettant de simuler les attaques de Jason et de déterminer le résultat du jeu
-function playRound() {
-    for (const survivant of survivants) {
-        if (jason.sante <= 0) {
+function playRound() 
+{
+    for (const survivant of survivants) 
+    {
+        if (jason.sante <= 0) 
+        {
             logMessage('Jason est mort');
-            logResult('<i class="fas fa-trophy"></i>');
             logMessage('Les survivants ont gagné, RIP à ' + survivants.map(s => s.nom).join(', '));
             return;
         }
         const resultat = attackSurvivant(survivant);
-        if (resultat === 'mort') {
+        if (resultat === 'mort') 
+        {
             survivants.splice(survivants.indexOf(survivant), 1);
         }
     }
 
-    if (survivants.length === 0) {
+    if (survivants.length === 0) 
+    {
         logMessage('Les survivants ont perdu. Jason les a tous tués.');
-        logResult('<i class="fas fa-skull"></i>');
     }
 }
 
 //Button permettant de commencer la chasse (start la boucle while)
-document.getElementById('start-button').addEventListener('click', function () {
+document.getElementById('start-button').addEventListener('click', function () 
+{
     jason.sante = 100;
     survivants = [];
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) 
+    {
         survivants.push({ nom: getRandomName(), caracteristique: getRandomCharacteristic() });
     }
-    logResult(''); // Réinitialiser les icônes de résultat
     logMessage('La chasse commence!');
-    while (jason.sante > 0 && survivants.length > 0) {
+    while (jason.sante > 0 && survivants.length > 0) 
+    {
         playRound();
     }
 });
